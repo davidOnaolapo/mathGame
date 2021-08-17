1564dae06dae3f0ebec628ddcb880fe4626e0b78
David Onaolapo <talk2dawich@gmail.com>
2021-08-17 00:30:18 +0000

    complete functionality

--- main.rb
+++ main.rb
@@ -20,11 +20,17 @@ player2 = Player.new(players[1])
 
 gameX = Game.new(player1);
 
-# while (!sessionX.game_over) do
+while (!sessionX.game_over) do
   sessionX.lets_play_round(player1, player2, gameX)
-# end
+end
 
-# sessionX.itsover;
+if(player1.lives_left == 0)
+  sessionX.its_over(player2, player1)
+else
+  sessionX.its_over(player1, player2)
+end
+
+
 
 
 
